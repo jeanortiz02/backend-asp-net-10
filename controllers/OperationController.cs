5d0f600171f38e97a6b48fa75fcfd65c2f3659a1
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2026-07-21 22:33:00 -0400

    first commit

---
+++ controllers/OperationController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Get(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public decimal Add( Numbers numbers)
+        {
+            return numbers.A - numbers.B;
+        }
+        [HttpPut]
+        public decimal Edit(decimal a, decimal b)
+        {
+            return a * b;
+        }
+        [HttpDelete]
+        public decimal Delete(decimal a, decimal b)
+        {
+            return a / b;
+        }
+    }
+}
+
+public class Numbers
+{
+    public decimal A { get; set;}
+    public decimal B     { get; set;}
+}
